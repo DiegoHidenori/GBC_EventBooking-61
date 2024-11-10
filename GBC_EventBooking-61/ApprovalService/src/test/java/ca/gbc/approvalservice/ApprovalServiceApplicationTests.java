420688c4b22aa5d5aa50acbc9a79ac588ccb7fa8
DiegoHidenori <diegohidenori.ca@hotmail.com>
2024-11-10 17:32:46 -0500

    After merging code for ApprovalService. Unsuccessful.

--- GBC_EventBooking-61/ApprovalService/src/test/java/ca/gbc/approvalservice/ApprovalServiceApplicationTests.java
+++ GBC_EventBooking-61/ApprovalService/src/test/java/ca/gbc/approvalservice/ApprovalServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.approvalservice;
+package ca.gbc.approvalservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -6,6 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
 import static org.assertj.core.api.Assertions.assertThat;
--- GBC_EventBooking-61/ApprovalService/src/test/java/ca/gbc/approvalservice/TestcontainersConfiguration.java
+++ GBC_EventBooking-61/ApprovalService/src/test/java/ca/gbc/approvalservice/TestcontainersConfiguration.java
@@ -1,4 +1,4 @@
-package com.example.approvalservice;
+package ca.gbc.approvalservice;
 
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
